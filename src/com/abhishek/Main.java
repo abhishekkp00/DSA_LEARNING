ba82a3e8c3f1da2ef0e1fa6b767c3e0fdd975e3e
abhishekkp00 <abhishekforcollege@gmail.com>
2025-09-10 20:31:24 +0530

    Conditional Basics

---
+++ src/com/abhishek/Main.java
@@ -0,0 +1,20 @@
+package com.abhishek;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+    public class Main {
+        public static void main(String[] args) {
+
+            System.out.println("Hello and welcome!");
+
+            //If statement
+            int Salary = 25000;
+            if (Salary>10000) {
+                System.out.println(Salary + 2000);
+            }
+            else{
+                    System.out.println(Salary + 1000);
+                }
+            }
+
+    }
