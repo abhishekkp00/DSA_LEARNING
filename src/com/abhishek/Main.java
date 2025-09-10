76338efa923ac6ba84df5789b549a9b977a64109
abhishekkp00 <abhishekforcollege@gmail.com>
2025-09-10 20:49:22 +0530

    Multiple if else

--- src/com/abhishek/Main.java
+++ src/com/abhishek/Main.java
@@ -1,5 +1,7 @@
 package com.abhishek;
 
+import java.lang.foreign.SegmentAllocator;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
     public class Main {
@@ -8,13 +10,31 @@ package com.abhishek;
             System.out.println("Hello and welcome!");
 
             //If statement
+
             int Salary = 25000;
-            if (Salary>10000) {
-                System.out.println(Salary + 2000);
+//            if (Salary>10000) {
+//                System.out.println(Salary + 2000);
+//            }
+//            else{
+//                    System.out.println(Salary + 1000);
+//                }
+
+
+            //Multiple if else
+
+            if (Salary > 20000) {
+                Salary += 4000;
+            } else if (Salary >= 15000) {
+                Salary += 2000;
             }
             else{
-                    System.out.println(Salary + 1000);
-                }
+                System.out.println("Nothing is there");
             }
+            System.out.println(Salary);
+        }
+}
 
-    }
+
+
+
+
