8fa7acc9ba928f2f90485d8dfec3e5078fd13cbb
abhishekkp00 <abhishekforcollege@gmail.com>
2025-09-11 11:51:07 +0530

    Name changed

--- src/com/abhishek/Conditionals.java
+++ src/com/abhishek/Conditionals.java
@@ -1,10 +1,8 @@
 package com.abhishek;
 
-import java.lang.foreign.SegmentAllocator;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-    public class Main {
+    public class Conditionals {
         public static void main(String[] args) {
 
             System.out.println("Hello and welcome!");
---
+++ src/com/abhishek/Loops.java
@@ -0,0 +1,4 @@
+package com.abhishek;
+
+public class Loops {
+}
